b7119fdc32a83ad3741a72b5cab7e5907fae2f01
Sourabh P <sourabhp94503@gmail.com>
2024-04-06 02:14:48 +0530

    Update: Submit delivery request

--- web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
+++ web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 
 const AddDelivery = () => {
@@ -12,16 +12,32 @@ const AddDelivery = () => {
     image: null,
   })
 
+  useEffect(() => {
+    console.log(deliveryData)
+  }, [deliveryData])
+
+  const onSubmit = async () => {
+
+  }
+
+  const handleImageInput = (e) => {
+    const imageFile = e.target.files[0]
+    setDeliveryData({ ...deliveryData, image: imageFile })
+  }
+
   return (
     <>
       <div className="add-delivery-container">
-        <form className='add-delivery-form'>
+        <form className='add-delivery-form' onSubmit={() => onSubmit()}>
           <h3>Fill the details of your request</h3>
-          <input type="text" name="name" value={deliveryData.name} onChange={(e) => setDeliveryData({ ...deliveryData, name: e.target.value })} placeholder='Enter name of the goods' />
-          <input type="text" name="source" value={deliveryData.source} onChange={(e) => setDeliveryData({ ...deliveryData, source: e.target.value })} placeholder='Enter source address coordinates' />
-          <input type="text" name="destination" value={deliveryData.destination} onChange={(e) => setDeliveryData({ ...deliveryData, destination: e.target.value })} placeholder='Enter destination address coordinates' />
-          <input type="text" name="goodsType" value={deliveryData.goodsType} onChange={(e) => setDeliveryData({ ...deliveryData, goodsType: e.target.value })} placeholder='Enter goods type' />
-          <textarea rows={5} cols={30} name="goodsDesription" value={deliveryData.goodsDesription} onChange={(e) => setDeliveryData({ ...deliveryData, goodsDesription: e.target.value })} placeholder='Enter description of goods' />
+          <input type="text" name="name" value={deliveryData.name} onChange={(e) => setDeliveryData({ ...deliveryData, name: e.target.value })} placeholder='Enter name of the goods' required />
+          <input type="text" name="source" value={deliveryData.source} onChange={(e) => setDeliveryData({ ...deliveryData, source: e.target.value })} placeholder='Enter source address coordinates' required />
+          <input type="text" name="destination" value={deliveryData.destination} onChange={(e) => setDeliveryData({ ...deliveryData, destination: e.target.value })} placeholder='Enter destination address coordinates' required />
+          <input type="text" name="goodsType" value={deliveryData.goodsType} onChange={(e) => setDeliveryData({ ...deliveryData, goodsType: e.target.value })} placeholder='Enter goods type' required />
+          <textarea rows={5} cols={30} name="goodsDesription" value={deliveryData.goodsDesription} onChange={(e) => setDeliveryData({ ...deliveryData, goodsDesription: e.target.value })} placeholder='Enter description of goods' required />
+          <input type="file" id="image" name="image" accept="image/*" onChange={(e) => { handleImageInput(e) }} required />
+          {deliveryData.image && <img src={URL.createObjectURL(deliveryData.image)} alt="uploadedImage" width={100} height={100} />}
+          <button className='add-delivery-request-button'>Submit</button>
         </form>
       </div>
     </>
