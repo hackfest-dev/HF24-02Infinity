c32790f552a680d1b8e8a7c14bbfa1bf04e0e9d7
Sourabh P <sourabhp94503@gmail.com>
2024-04-06 02:21:47 +0530

    Add: API call to add delivery-request

--- web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
+++ web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
@@ -12,12 +12,41 @@ const AddDelivery = () => {
     image: null,
   })
 
+  const [message, setMessage] = useState('')
+
   useEffect(() => {
     console.log(deliveryData)
   }, [deliveryData])
 
-  const onSubmit = async () => {
+  const onSubmit = async (e) => {
+    e.preventDefault()
 
+    setDeliveryData({ ...deliveryData, image: URL.createObjectURL(deliveryData.image) })
+
+    const response = await fetch(
+      "http://localhost:5001/api/delivery/newrequest",
+      {
+        method: "POST",
+        headers: {
+          Accept: "Application/json",
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify({
+          ...deliveryData,
+        }),
+      }
+    )
+
+    const data = await response.json()
+
+    if (data.status === 200) {
+      setMessage(data.message + 'You will recieve notification, once approved...')
+      setTimeout(() => {
+        setMessage('')
+      }, 5000)
+    } else {
+      setMessage(data.message)
+    }
   }
 
   const handleImageInput = (e) => {
@@ -28,7 +57,7 @@ const AddDelivery = () => {
   return (
     <>
       <div className="add-delivery-container">
-        <form className='add-delivery-form' onSubmit={() => onSubmit()}>
+        <form className='add-delivery-form' onSubmit={(e) => onSubmit(e)}>
           <h3>Fill the details of your request</h3>
           <input type="text" name="name" value={deliveryData.name} onChange={(e) => setDeliveryData({ ...deliveryData, name: e.target.value })} placeholder='Enter name of the goods' required />
           <input type="text" name="source" value={deliveryData.source} onChange={(e) => setDeliveryData({ ...deliveryData, source: e.target.value })} placeholder='Enter source address coordinates' required />
@@ -37,6 +66,7 @@ const AddDelivery = () => {
           <textarea rows={5} cols={30} name="goodsDesription" value={deliveryData.goodsDesription} onChange={(e) => setDeliveryData({ ...deliveryData, goodsDesription: e.target.value })} placeholder='Enter description of goods' required />
           <input type="file" id="image" name="image" accept="image/*" onChange={(e) => { handleImageInput(e) }} required />
           {deliveryData.image && <img src={URL.createObjectURL(deliveryData.image)} alt="uploadedImage" width={100} height={100} />}
+          <p className='add-delivery-request-message'>{message}</p>
           <button className='add-delivery-request-button'>Submit</button>
         </form>
       </div>
