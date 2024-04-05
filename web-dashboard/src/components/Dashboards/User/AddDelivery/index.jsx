b88890c2485ed21034d6450b6d974a43d6652217
Sourabh P <sourabhp94503@gmail.com>
2024-04-06 01:56:12 +0530

    Update: AddDeliveryRequest Form

--- web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
+++ web-dashboard/src/components/Dashboards/User/AddDelivery/index.jsx
@@ -17,11 +17,11 @@ const AddDelivery = () => {
       <div className="add-delivery-container">
         <form className='add-delivery-form'>
           <h3>Fill the details of your request</h3>
-          <input type="text" name="name" value={deliveryData.name} onChange={(e) => setDeliveryData({ ...deliveryData, name: e.target.value })} />
-          <input type="text" name="source" value={deliveryData.source} onChange={(e) => setDeliveryData({ ...deliveryData, source: e.target.value })} />
-          <input type="text" name="destination" value={deliveryData.destination} onChange={(e) => setDeliveryData({ ...deliveryData, destination: e.target.value })} />
-          <input type="text" name="goodsType" value={deliveryData.goodsType} onChange={(e) => setDeliveryData({ ...deliveryData, goodsType: e.target.value })} />
-          <textarea rows={10} cols={30} name="goodsDesription" value={deliveryData.goodsDesription} onChange={(e) => setDeliveryData({ ...deliveryData, goodsDesription: e.target.value })} />
+          <input type="text" name="name" value={deliveryData.name} onChange={(e) => setDeliveryData({ ...deliveryData, name: e.target.value })} placeholder='Enter name of the goods' />
+          <input type="text" name="source" value={deliveryData.source} onChange={(e) => setDeliveryData({ ...deliveryData, source: e.target.value })} placeholder='Enter source address coordinates' />
+          <input type="text" name="destination" value={deliveryData.destination} onChange={(e) => setDeliveryData({ ...deliveryData, destination: e.target.value })} placeholder='Enter destination address coordinates' />
+          <input type="text" name="goodsType" value={deliveryData.goodsType} onChange={(e) => setDeliveryData({ ...deliveryData, goodsType: e.target.value })} placeholder='Enter goods type' />
+          <textarea rows={5} cols={30} name="goodsDesription" value={deliveryData.goodsDesription} onChange={(e) => setDeliveryData({ ...deliveryData, goodsDesription: e.target.value })} placeholder='Enter description of goods' />
         </form>
       </div>
     </>
