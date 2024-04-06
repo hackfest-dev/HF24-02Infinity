dd506b1423ba4b01fc8a6ca79ba518d5291ef5b4
Sourabh P <sourabhp94503@gmail.com>
2024-04-06 22:27:02 +0530

    Update: ML model saving API

--- Server/controllers/mlDataSavingController.js
+++ Server/controllers/mlDataSavingController.js
@@ -3,44 +3,40 @@ const asyncHandler = require("express-async-handler");
 const liveDeliveryModel = require("../models/liveDeliveryModel");
 
 const mlDataSync = asyncHandler(async (req, res) => {
-  const { latitude, longitude, speed, anomalies, penalty } = req.body;
-  const driverId = "661051338d2a8c278ee76da3";
+  const { latitude, longitude, averageSpeed, anomalies, penalty, driverId, speedViolation } = req.body
   try {
-    const result = await liveDeliveryModel.findOne({ driverId: driverId });
+    const delivery = await liveDeliveryModel.findOne({ driverId: driverId })
 
-    result.speed = speed;
-    if (speed > 60) {
-      result.speed = result.speed + 1;
+    if(delivery) {
+      return res
+      .status(400)
+      .json({ message: "Check your id", status: 400 })
     }
-    result.anomalies += anomalies;
-    result.currentLocation = `${latitude},${longitude}`;
-    // result.penalty=result.penalty+penalty
-    // await result.save();
-    result.save(function (err, savedDoc) {
-      if (err) {
-        return res
+
+    delivery.anomalies += anomalies
+    delivery.currentLocation = `${latitude},${longitude}`
+    delivery.avgSpeed = averageSpeed
+    delivery.penalty = penalty
+    delivery.speedViolation = speedViolation
+    
+    const updated = await delivery.save()
+
+    if(updated === null){
+      return res
+      .status(500)
+      .json({ message: "Failed to sync data", status: 500 })
+    }else {
+      return res
           .status(200)
-          .json({ message: "data synced successfully", status: 200 });
-      } else {
-        return res
-          .status(500)
-          .json({ message: "Failed to sync data", status: 500 });
-      }
-    });
-    // if(result.modifiedCount==1){
-    //     return res.status(200).json({ message: 'data synced successfully', status: 200 })
-    // } else {
-    //     return res.status(500).json({ message: 'Failed to sync data', status: 500 })
-    // }
-    return res
-      .status(200)
-      .json({ message: "data synced successfully", status: 200 });
+          .json({ message: "data synced successfully", status: 200 })
+    }
+
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error", status: 500 });
+    console.error(err)
+    res.status(500).json({ message: "Internal server error", status: 500 })
   }
-});
+})
 
 module.exports = {
-  mlDataSync,
-};
+  mlDataSync
+}
