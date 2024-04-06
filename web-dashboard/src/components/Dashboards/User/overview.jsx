3f1dd2c6621944721ecb1ec76e99b847781e7dd3
Sourabh P <sourabhp94503@gmail.com>
2024-04-06 21:49:38 +0530

    Update: Users Dashboard

--- web-dashboard/src/components/Dashboards/User/overview.jsx
+++ web-dashboard/src/components/Dashboards/User/overview.jsx
@@ -4,26 +4,32 @@ import AddDelivery from './AddDelivery';
 import MapComponent from '../Driver/DirectionMap';
 import GetPlaceName from '../Driver/maps'
 
-const Overview = ({ totaldriver, totalcustomer, ongoingfleet }) => {
+const Overview = () => {
+
     return (
         <div className='overview-container'>
 
             <div className="overview">
                 <div className="card">
-                    {/* <IoIosPerson /> */}
                     <div className="card-content1">
                         <h3>Logistics Status</h3>
                         <p>3</p>
                     </div>
                 </div>
-
             </div>
             <div className="overview">
                 <div className="card">
-                    {/* <IoIosPerson /> */}
                     <div className="card-content1">
                         <h3>Current Location</h3>
                         <p><GetPlaceName/></p>
+                    </div>
+                </div>
+            </div>
+            <div className="overview">
+                <div className="card">
+                    <div className="card-content1">
+                        <h3>Driver Details</h3>
+                        <p>3</p>
                     </div>
                 </div>
             </div>
